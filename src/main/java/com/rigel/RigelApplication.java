0dfe74f24ed244c3713b53ca1dc0c684f470d3a8
thimiracool <thimirac@gmail.com>
2017-02-26 16:43:23 +0000

    Add Spring-boot Project

---
+++ src/main/java/com/rigel/RigelApplication.java
@@ -0,0 +1,12 @@
+package com.rigel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RigelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RigelApplication.class, args);
+	}
+}
